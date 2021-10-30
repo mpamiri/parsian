18fba6d090a6847df1f026e4bc38b50433369b6c
parsa <m.p.amiri84@gmail.com>
2021-10-30 21:12:31 +0330

    commit

--- parsian/parsian/urls.py
+++ parsian/parsian/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('./main/',include('urls'))
+    path('main/',include('parsian.parsian.urls'))
 ]
--- parsian/parsianapp/urls.py
+++ parsian/parsianapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns =[
-    path('main/start',views.first)
+    path('start/',views.first)
 ]
