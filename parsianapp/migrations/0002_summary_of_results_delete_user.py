ffd99a7c40f95ae9b124c1125ef99d2f4c22a463
parsa <m.p.amiri84@gmail.com>
2022-01-30 09:13:57 -0800

    commit

--- parsianapp/migrations/0002_summary_of_results_delete_user.py
+++ parsianapp/migrations/0002_summary_of_results_delete_user.py
@@ -5,9 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('parsianapp', '0001_initial'),
-    ]
+
 
     operations = [
         migrations.CreateModel(
