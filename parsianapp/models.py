d7535cdf988dcc703d65cdd828901d85700b2142
parsa <m.p.amiri84@gmail.com>
2022-02-02 11:47:40 -0800

    commit

--- parsianapp/models.py
+++ parsianapp/models.py
@@ -7,8 +7,8 @@ class summary_of_results(models.Model):
     start_year = models.IntegerField(default=0)
     job = models.CharField(max_length=30)
     harmful_factors = models.CharField(max_length=100)
-    CODE_CHOICES=[('1'),('2'),('3')]
-    code = forms.ChoiceField(choices=CODE_CHOICES, widget=forms.RadioSelect)
+    CODE_CHOICES=[(1,'1'),(2,'2'),(3,'3')]
+    code = models.IntegerField(default=0,choices=CODE_CHOICES)
     age = models.IntegerField(default=0)
     length = models.IntegerField(default=0)
     weight = models.IntegerField(default=0)
@@ -22,40 +22,51 @@ class summary_of_results(models.Model):
     cholesterol_status = models.CharField(max_length=300)
     triglyceride = models.IntegerField(default=0)
     triglyceride_status = models.CharField(max_length=300)
-    urine_CHOICES=[('نرمال'),('غیر نرمال'),('ندارد')]
-    urine = forms.ChoiceField(choices=urine_CHOICES, widget=forms.RadioSelect)
-    breath_CHOICES=[('نرمال'),('تحدیدی'),('انسدادی'),('نیاز به تکرار'),('نمای توامان تحدیدی وانسدادی'),('ندارد'),]
-    breathing_test = forms.ChoiceField(choices=breath_CHOICES, widget=forms.RadioSelect)
-    left_ear_hearing = models.CharField(max_length=300)
-    right_ear_hearing = models.CharField(max_length=300)
-    left_eye_vision = models.CharField(max_length=300)
+    URINE_CHOICES=[(normal,'نرمال'),(not_normal,'غیر نرمال'),(none,'ندارد')]
+    urine = models.CharField(choices=URINE_CHOICES)
+    breathing_CHOICES=[(normal,'نرمال'),(tahdidi,'تحدیدی'),(ensedadi,'انسدادی'),(niaz_be_tekrar,'نیاز به تکرار'),(namaie_toaman,'نمای توامان تحدیدی وانسدادی'),(none,'ندارد')]
+    breathing_test = models.CharField(choices=breathing_CHOICES)
+    left_ear_CHOICES=[('نرمال'),('کاهش شنوایی هدایتی'),('کاهش شنوایی حسی عصبی'),('کاهش شنوایی ناشی از صدا'),('توامان هدایتی و حسی عصبی'),('ندارد')]
+    left_ear_hearing = models.CharField(choices=breathing_CHOICES)
+    right_ear_CHOICES=[('نرمال'),('کاهش شنوایی هدایتی'),('کاهش شنوایی حسی عصبی'),('کاهش شنوایی ناشی از صدا'),('توامان هدایتی و حسی عصبی'),('ندارد')]
+    right_ear_hearing = models.CharField(choices=breathing_CHOICES)
+    left_eye_vision = models.IntegerField(default=0)
     left_eye_vision_status = models.CharField(max_length=300)
-    left_eye_vision_with_glasses = models.CharField(max_length=300)
-    right_eye_vision = models.CharField(max_length=300)
+    left_eye_vision_with_glasses = models.IntegerField(default=0)
+    right_eye_vision = models.IntegerField(default=0)
     right_eye_vision_status = models.CharField(max_length=300)
-    right_eye_vision_with_glasses = models.CharField(max_length=300)
-    color_vision = models.CharField(max_length=300)
-    field_of_veiw = models.CharField(max_length=300)
-    depth_vision_status = models.CharField(max_length=300)
-    breast_photo = models.CharField(max_length=300)
-    blood_lead = models.CharField(max_length=300)
+    right_eye_vision_with_glasses = models.IntegerField(default=0)
+    CODE_CHOICES=[('نرمال'),('غیر نرمال'),('تشخیص رنگ قرمز'),('ندارد')]
+    color_vision = models.CharField(choices=breathing_CHOICES)
+    CODE_CHOICES=[('نرمال'),('غیر نرمال'),('ندارد')]
+    field_of_veiw = models.CharField(choices=breathing_CHOICES)
+    CODE_CHOICES=[('نرمال'),('غیر نرمال'),('ندارد')]
+    depth_vision = models.CharField(choices=breathing_CHOICES)
+    CODE_CHOICES=[('نرمال'),('غیر نرمال'),('ندارد')]
+    breast_photo = models.CharField(choices=breathing_CHOICES)
+    CODE_CHOICES=[('نرمال'),('تغییرات غیر اختصاصی'),('نیاز به تکرار'),('غیر نرمال'),('ندارد')]
+    blood_lead = models.CharField(choices=breathing_CHOICES)
     blood_lead_status = models.CharField(max_length=300)
-    PSA = models.CharField(max_length=300)
+    PSA = models.IntegerField(default=0)
     PSA_status = models.CharField(max_length=300)
-    TSH = models.CharField(max_length=300)
+    TSH = models.IntegerField(default=0)
     TSH_status = models.CharField(max_length=300)
     problem = models.CharField(max_length=300)
-    problems_with_the_persons_job = models.CharField(max_length=300)
+    CODE_CHOICES=[('دارد'),('ندارد')]
+    problems_with_the_persons_job = models.CharField(choices=breathing_CHOICES)
     recommendations = models.CharField(max_length=300)
-    refer_to_specialist = models.CharField(max_length=300)
-    final_theory = models.CharField(max_length=300)
+    CODE_CHOICES=[('ندارد'),('قلب و عروق'),('داخلی'),('ریه'),('غدد'),('سایر')]
+    refer_to_specialist = models.CharField(choices=breathing_CHOICES)
+    CODE_CHOICES=[('بلامانع'),('تغییر شغل'),('مشروط'),('کمیسیون')]
+    final_theory = models.CharField(choices=breathing_CHOICES)
     reson_for_opening_the_case = models.CharField(max_length=300)
-    occupational_actions = models.CharField(max_length=300)
+    CODE_CHOICES=[('درحال پیگیری'),('انجام شده'),('انجام نشده')]
+    occupational_actions = models.CharField(choices=breathing_CHOICES)
     occupational_disease_code = models.CharField(max_length=300)
-    AST = models.CharField(max_length=300)
+    AST = models.IntegerField(default=0)
     AST_status = models.CharField(max_length=300)
-    ALT = models.CharField(max_length=300)
+    ALT = models.IntegerField(default=0)
     ALT_status = models.CharField(max_length=300)
-    age_two = models.CharField(max_length=300)
+    age_two = models.IntegerField(default=0)
     duration_of_employment = models.CharField(max_length=300)
     overweight = models.CharField(max_length=300)
