7c352dfebe93b3c40250a8fc469aa5818500b271
parsa <m.p.amiri84@gmail.com>
2022-02-25 12:18:17 -0800

    commit

--- parsianapp/urls.py
+++ parsianapp/urls.py
@@ -14,6 +14,13 @@ urlpatterns =[
     path('manage/summary_of_results/add_summary/',views.addsummary,name='addsummary'),
     path('manage/submit_company/',views.company,name='submit_company'),
     path('manage/occupational_disease/',views.occupational_disease,name='occupational_disease'),
+    path('manage/occupational_disease/disease_code',views.disease_code,name='disease_code'),
+    path('manage/occupational_disease/open_docs',views.open_docs,name='open_docs'),
+    path('manage/occupational_disease/summary_of_examinations',views.summary_of_examinations,name='summary_of_examinations'),
+    path('manage/occupational_disease/problem',views.problem,name='problem'),  
+    path('manage/occupational_disease/specialist',views.specialist,name='specialist'),
+    path('manage/occupational_disease/graph',views.graph,name='graph'),  
+    path('manage/occupational_disease/solo_output',views.solo_output,name='solo_output'),  
     path('manage/submit_company/add_company',views.addcompany,name='addcompany')    
 ]
 urlpatterns += staticfiles_urlpatterns()
