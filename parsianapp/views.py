2333e78e89b323a41660943b40ea3f9f914b51c4
parsa <m.p.amiri84@gmail.com>
2022-08-30 22:13:21 -0700

    commit

--- parsianapp/views.py
+++ parsianapp/views.py
@@ -212,7 +212,7 @@ def disease_pdf_view(request):
     personal_species=Personal_Species_Model.objects.filter(examinations_code=examinations_course)
     options = Options()
     options.headless = True
-    river = webdriver.Chrome(ChromeDriverManager().install(),options=options)
+    driver = webdriver.Chrome(executable_path='./chromedriver',options=options)
     driver.get("http://127.0.0.1:8000/login")
     username = driver.find_element('name',"username")
     password = driver.find_element('name',"password")
@@ -297,7 +297,7 @@ def open_docs_pdf_view(request):
     personal_species=Personal_Species_Model.objects.filter(examinations_code=examinations_course,final__mashrot='False',final__belamane='False',final__rad='False')
     options = Options()
     options.headless = True
-    river = webdriver.Chrome(executable_path='./chromedriver.exe',options=options)
+    driver = webdriver.Chrome(executable_path='./chromedriver',options=options)
     driver.get("http://127.0.0.1:8000/login")
     username = driver.find_element('name',"username")
     password = driver.find_element('name',"password")
@@ -383,7 +383,7 @@ def summary_of_examinations_pdf_view(request):
     personal_species=Personal_Species_Model.objects.filter(examinations_code=examinations_course)
     options = Options()
     options.headless = True
-    river = webdriver.Chrome(executable_path='./chromedriver.exe',options=options)
+    driver = webdriver.Chrome(executable_path='./chromedriver',options=options)
     driver.get("http://127.0.0.1:8000/login")
     username = driver.find_element('name',"username")
     password = driver.find_element('name',"password")
@@ -467,7 +467,7 @@ def problem_pdf_view(request):
     personal_species=Personal_Species_Model.objects.filter(examinations_code=examinations_course)
     options = Options()
     options.headless = True
-    driver = webdriver.Chrome(executable_path='./chromedriver.exe',options=options)
+    driver = webdriver.Chrome(executable_path='./chromedriver',options=options)
     driver.get("http://127.0.0.1:8000/login")
     username = driver.find_element('name',"username")
     password = driver.find_element('name',"password")
@@ -856,7 +856,7 @@ def graph_view(request):
 def graph_pdf_view(request):
     options = Options()
     options.headless = True
-    river = webdriver.Chrome(executable_path='./chromedriver.exe',options=options)
+    driver = webdriver.Chrome(executable_path='./chromedriver',options=options)
     driver.get("http://127.0.0.1:8000/login")
     username = driver.find_element('name',"username")
     password = driver.find_element('name',"password")
@@ -945,7 +945,7 @@ def solo_pdf_view(request):
     personal_species=Personal_Species_Model.objects.filter(examinations_code=examinations_course)
     options = Options()
     options.headless = True
-    river = webdriver.Chrome(executable_path='./chromedriver.exe',options=options)
+    driver = webdriver.Chrome(executable_path='./chromedriver',options=options)
     driver.get("http://127.0.0.1:8000/login")
     username = driver.find_element('name',"username")
     password = driver.find_element('name',"password")
@@ -1740,7 +1740,7 @@ def examinations_output_course_pdf_view(request):
     personal_species=Personal_Species_Model.objects.filter(examinations_code=examinations_course)
     options = Options()
     options.headless = True
-    river = webdriver.Chrome(executable_path='./chromedriver.exe',options=options)
+    driver = webdriver.Chrome(executable_path='./chromedriver',options=options)
     driver.get("http://127.0.0.1:8000/login")
     username = driver.find_element('name',"username")
     password = driver.find_element('name',"password")
@@ -1825,7 +1825,7 @@ def examinations_output_person_pdf_view(request):
         personal_species = []
     options = Options()
     options.headless = True
-    river = webdriver.Chrome(executable_path='./chromedriver.exe',options=options)
+    driver = webdriver.Chrome(executable_path='./chromedriver',options=options)
     driver.get("http://127.0.0.1:8000/login")
     username = driver.find_element('name',"username")
     password = driver.find_element('name',"password")
